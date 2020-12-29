7984d19a7b0b59f8bee880c1bd7e3913516e8c15
Sanidhya Pawar <37259966+pawarss1@users.noreply.github.com>
2020-12-29 20:37:36 +0530

    Final Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,145 +1,149 @@
-import React, { Component, useEffect, useRef, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "../styles/App.css";
 
 const App = () => {
-  const [time, setTime] = useState("Work-Time");
-  const [workTime, setWorkTime] = useState(25);
-  const [breakTime, setBreakTime] = useState(5);
-  const [minutes, setMinutes] = useState(workTime);
-  const [seconds, setSeconds] = useState(0);
-  const [start, setStart] = useState(false);
-  const [stop, setStop] = useState(true);
-  const [reset, setReset] = useState(true);
-  const [set, setSet] = useState(false);
-  const workInput = useRef(null);
-  const breakInput = useRef(null);
-
+  let [curTask, setTask] = useState(false);
+  let [resetFlag, setResetFlag] = useState(false);
+  let [stopFlag, setStopFlag] = useState(false);
+  let [startFlag, setStartFlag] = useState(false);
+  let [workTime, setWorkTime] = useState(25);
+  let [breakTime, setBreakTime] = useState(5);
+  let [timerString, setTimer] = useState("25:00");
+  let [secondsRemain, setSecondRemain] = useState(1500);
   let intervalId = null;
-
-  const tick = () => {
-    if (stop) return;
-    if (seconds > 0) {
-      setSeconds(seconds - 1);
-    }
-    if (seconds === 0) {
-      if (minutes === 0) {
-        clearInterval(intervalId);
-        if (time === "Work-Time") {
-          setMinutes(breakTime);
-          setSeconds(0);
-          setTime("Break-Time");
-          alert("work duration is over");
-        } else {
-          setMinutes(workTime);
-          setSeconds(0);
-          setTime("Work-Time");
-          alert("break duration is over");
-        }
-      } else {
-        setMinutes(minutes - 1);
-        setSeconds(59);
-      }
-    }
-    // console.log(minutes, seconds);
-  };
-
-  const setHandler = () => {
-    if (workTime === 0 && breakTime === 0) {
-      resetHandler();
-      return;
-    }
-    setReset(false);
-    setSet(false);
-    setStart(false);
-    setStop(true);
-    setMinutes(workTime);
-    setSeconds(0);
-    setTime("Work-Time");
-  };
-
-  const startHandler = () => {
-    setStart(true);
-    setStop(false);
-    setReset(false);
-    setSet(true);
-  };
-
-  const stopHandler = () => {
-    setStop(true);
-    setStart(false);
-    setReset(false);
-    setSet(false);
-  };
-
-  const resetHandler = () => {
-    setReset(true);
-    setStop(true);
-    setStart(false);
-    setSet(false);
-    setTime("Work-Time");
-    setWorkTime(25);
-    setBreakTime(5);
-    setMinutes(25);
-    setSeconds(0);
-    // console.log(workInput);
-    workInput.current.value = 25;
-    breakInput.current.value = 5;
-  };
-
+  //false for worktime and true for break time.
   useEffect(() => {
-    console.log("here");
-    intervalId = setInterval(tick, 1000);
+    intervalId = setInterval(getFormatedTime, 1 * 1000);
     return () => clearInterval(intervalId);
   });
-
+  function updateWorkTime(event) {
+    if (event.target.value < 0) {
+      setWorkTime(25);
+      return;
+    }
+    setWorkTime(event.target.value);
+  }
+  function updateBreakTime(event) {
+    if (event.target.value < 0) {
+      setBreakTime(5);
+      return;
+    }
+    setBreakTime(event.target.value);
+  }
+  function setDurations() {
+    if (workTime == 0 && breakTime == 0) {
+      setWorkTime(25);
+      setBreakTime(5);
+      setTimer(`25:00`);
+    } else {
+      const temp = workTime < 10 ? `0${workTime}` : workTime;
+      setTimer(`${temp}:00`);
+    }
+    setSecondRemain(60 * workTime);
+  }
+  function getFormatedTime() {
+    if (!stopFlag && startFlag) {
+      let minuteNow = Math.floor(secondsRemain / 60);
+      let secNow = secondsRemain % 60;
+      minuteNow = minuteNow < 10 ? `0${minuteNow}` : minuteNow;
+      secNow = secNow < 10 ? `0${secNow}` : secNow;
+      if (minuteNow === "00" && secNow === "00") {
+        //clearInterval(intervalId);
+        //intervalId = null;
+        console.log(curTask);
+        curTask == false
+          ? alert("work duration is over")
+          : alert("break duration is over");
+        if (curTask == false) {
+          //worktime
+          const temp = breakTime < 10 ? `0${breakTime}` : breakTime;
+          setTimer(`${temp}:00`);
+          setSecondRemain(60 * breakTime);
+        } else {
+          //break time
+          const temp = workTime < 10 ? `0${workTime}` : workTime;
+          setTimer(`${temp}:00`);
+          setSecondRemain(60 * workTime);
+        }
+        setTask(!curTask);
+        return;
+      }
+      setTimer(`${minuteNow}:${secNow}`);
+      setSecondRemain(secondsRemain - 1);
+      //secondsRemain = Math.max(secondsRemain - 1, 0);
+    }
+  }
+  function startTimer() {
+    setStopFlag(false);
+    setStartFlag(true);
+    setResetFlag(false);
+  }
+  function stopTimer() {
+    setStartFlag(false);
+    setStopFlag(true);
+    setResetFlag(false);
+  }
+  function resetTimer() {
+    setStartFlag(false);
+    setStopFlag(true);
+    setResetFlag(true);
+    setWorkTime(25);
+    setBreakTime(5);
+    setTimer(`25:00`);
+    setSecondRemain(1500);
+  }
+  
   return (
     <div id="main" className="container">
-      <div className="clock">
-        <h1 className="timer">
-          {minutes < 10 ? `0${minutes}` : minutes}:
-          {seconds < 10 ? `0${seconds}` : seconds}
-        </h1>
-        <h3>{time}</h3>
+      <div>
+        <h1>{timerString}</h1>
+        {!curTask && (
+          <p>
+            <b>Work-Time</b>
+          </p>
+        )}
+        {curTask && (
+          <p>
+            <b>Break-Time</b>
+          </p>
+        )}
       </div>
-      <div className="control">
-        <button data-testid="start-btn" disabled={start} onClick={startHandler}>
-          Start
-        </button>
-        <button data-testid="stop-btn" disabled={stop} onClick={stopHandler}>
-          Stop
-        </button>
-        <button data-testid="reset-btn" disabled={reset} onClick={resetHandler}>
-          Reset
-        </button>
-      </div>
-      <br />
-      <div className="parameters">
-        <form onSubmit={(evt) => evt.preventDefault()}>
+      <div>
+        <div>
+          <button data-testid="start-btn" onClick={startTimer} disabled={startFlag}>
+            Start
+          </button>
+          <button data-testid="stop-btn" onClick={stopTimer} disabled={stopFlag}>Stop</button>
+          <button data-testid="reset-btn" onClick={resetTimer} disabled={resetFlag}>Reset</button>
+        </div>
+
+        <div className="timer-grp">
           <input
-            placeholder="work duration"
+            type="number"
             data-testid="work-duration"
-            type="Number"
-            onChange={(evt) => setWorkTime(Number(evt.target.value))}
-            defaultValue={workTime}
-            disabled={set}
-            ref={workInput}
+            placeholder="work duration"
+            value={workTime}
+            onChange={(event) => {
+              updateWorkTime(event);
+            }}
             required
-          />
+            disabled={startFlag}
+          ></input>
           <input
-            placeholder="break duration"
+            type="number"
             data-testid="break-duration"
-            type="Number"
+            placeholder="break duration"
+            value={breakTime}
+            onChange={(event) => {
+              updateBreakTime(event);
+            }}
             required
-            onChange={(evt) => setBreakTime(Number(evt.target.value))}
-            defaultValue={breakTime}
-            disabled={set}
-            ref={breakInput}
-            required
-          />
-          <button data-testid="set-btn" disabled={set} onClick={setHandler}>
+            disabled={startFlag}
+          ></input>
+          <button data-testid="set-btn" onClick={setDurations} disabled={startFlag}>
             Set
           </button>
-        </form>
+        </div>
       </div>
     </div>
   );
